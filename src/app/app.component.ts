cd5586e1f2e8671ee39d1ea44b392b05edff281d
Peter <youngeunpc@yahoo.com>
2025-03-15 16:06:44 -0300

    Listening for changes to ngModel values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,32 @@ import { WishItem } from '../shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('Visit Japan', false),
-    // new WishItem('Go to the gym', false),
-    // new WishItem('Go eat infinite sushi', true)
+    new WishItem('Visit Japan', false),
+    new WishItem('Go to the gym', false),
+    new WishItem('Go eat infinite sushi', true)
   ];
+
+  listFilter : String = '0';
 
   newWishText = '';
 
   title = 'wishlist';
 
+  visibleItems : WishItem[] = this.items;
+
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
+  };
+
+  filterChanged(value : any) {
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
   };
 
   toggleItem(item : WishItem) {
