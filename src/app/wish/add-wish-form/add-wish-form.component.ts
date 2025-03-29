9c0886ea0f54547fc7997542e5cb91bc2c0fae84
Peter <youngeunpc@yahoo.com>
2025-03-28 21:12:05 -0300

    Creating modules

--- src/app/wish/add-wish-form/add-wish-form.component.ts
+++ src/app/wish/add-wish-form/add-wish-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { WishItem } from '../../shared/models/wishItem';
+import { WishItem } from '../../../shared/models/wishItem';
 
 @Component({
   selector: 'add-wish-form',
